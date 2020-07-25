ebedeacb3a98af19df4cbeafca9b0b7dc7a73306
Jaime Hernandez <jaimevazz@gmail.com>
2020-07-25 01:21:25 -0500

    Updated media and static dirs

--- doriscience/doriscience/settings.py
+++ doriscience/doriscience/settings.py
@@ -137,10 +137,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'doriscience/static/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
